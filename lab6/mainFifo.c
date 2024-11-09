3be7f80e9b0d1dfc9b1a434be30693abaca539bb
denis-anisimov57 <den.anisimov.05@mail.ru>
2024-11-10 01:59:44 +0300

    Added fifo between different processes

--- lab6/mainFifo.c
+++ lab6/mainFifo.c
@@ -8,72 +8,53 @@
 #include <string.h>
 #include <errno.h>
 
+const size_t size = 64;
+
+char* getStrTime() {
+	time_t ts = time(NULL);
+	struct tm curTime = *localtime(&ts);
+	char* timeStr = (char*)calloc(1, size);
+	strftime(timeStr, size + 1, "%H:%M:%S;", &curTime);
+	return timeStr;
+}
+
 int main(int argc, char** argv) {
 	(void)argc, (void)argv;
 	const char* fifoName = "fifoLab";
-	int fifoRes = mkfifo(fifoName, S_IRUSR | S_IWUSR);	
-	if(fifoRes != 0) {
-		int err = errno;
-		fprintf(stderr, "Fifo error: %s (%d)\n", strerror(err), err);
-		return 1;
-	}
-	pid_t res;
-	switch(res = fork()) {
-		case -1: {
+	int fifoD = open(fifoName, O_RDONLY);
+	if(fifoD == -1) {
+		int fifoRes = mkfifo(fifoName, S_IRUSR | S_IWUSR);	
+ 		if(fifoRes != 0) {
 			int err = errno;
-			fprintf(stderr, "Fork error: %s (%d)\n", strerror(err), err);
-			break;
+			fprintf(stderr, "Fifo error: %s (%d)\n", strerror(err), err);
+			return 1;
 		}
-		case 0: {
-			size_t size = 64;
-			char buf[size];
-
-			time_t ts = time(NULL);
-			struct tm curTime = *localtime(&ts);
-			char* timeStr = (char*)calloc(1, size);
-			strftime(timeStr, size + 1, "Child time: %H:%M:%S;\n", &curTime);	
-			printf("%s", timeStr);
-			free(timeStr);
-
-			int fd = open(fifoName, O_RDONLY);
-			if(fd == -1) {
-				fprintf(stderr, "Open fifo error\n");
-				return 1;
-			}
-			int len = 0;
-			while((len = read(fd, buf, sizeof(buf))) != 0) {
-				write(2, buf, len);
-			}
-			close(fd);
-
-			break;	
-		}
-		default: {
-			sleep(6);
-			time_t ts = time(NULL);
-			size_t size = 64;
-			struct tm curTime = *localtime(&ts);
-			char* timeStr = (char*)calloc(1, size);
-			strftime(timeStr, size + 1, "Parent time: %H:%M:%S; ", &curTime);
+		fifoD = open(fifoName, O_WRONLY);
+		for(int i = 0; i < 2; i++) {
+			char* pidStr = (char*)calloc(1, 2 * size);
+			sprintf(pidStr, "Process %d; Time: ", getpid());
 			
-			char* pidStr = (char*)calloc(1, size);
-			sprintf(pidStr, "Parent pid: %d\n", getpid());
-			strcat(timeStr, pidStr);
-			free(pidStr);
-
-			int fd = open(fifoName, O_WRONLY);
-			if(fd == -1) {
-				fprintf(stderr, "Open fifo error\n");
-				free(timeStr);
-				return 1;
-			}
-			write(fd, timeStr, strlen(timeStr) + 1);
+			char* timeStr = getStrTime();
+			sleep(5);
+			strcat(pidStr, timeStr);
+				
+			write(fifoD, pidStr, size + 1);
 			free(timeStr);
-			close(fd);
-					
-			break;
+			free(pidStr);
 		}
+		close(fifoD);
 	}
-	unlink(fifoName);
+	else {
+		int len;
+		char* buf = (char*)calloc(1, size);
+		while((len = read(fifoD, buf, size)) != 0) {
+			char* timeStr = getStrTime();
+			printf("Current process %d, time %s ------ String from: \"%s\"\n", getpid(), timeStr, buf);	
+			free(timeStr);
+		}
+		free(buf);
+		close(fifoD);
+	}
+	unlink(fifoName);		
 	return 0;
 }
